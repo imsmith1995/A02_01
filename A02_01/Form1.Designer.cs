05639b8e1792e472d7147d771e22db037d7ff862
Ian Smith <imsmith1995@gmail.com>
2023-06-23 15:35:14 -0400

    Add project files.

---
+++ A02_01/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace A02_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Address = new System.Windows.Forms.Label();
+            this.lbl_City = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(126, 114);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(192, 99);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Personal Info";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Name.Location = new System.Drawing.Point(671, 151);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(0, 25);
+            this.lbl_Name.TabIndex = 1;
+            // 
+            // lbl_Address
+            // 
+            this.lbl_Address.AutoSize = true;
+            this.lbl_Address.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_Address.Location = new System.Drawing.Point(671, 342);
+            this.lbl_Address.Name = "lbl_Address";
+            this.lbl_Address.Size = new System.Drawing.Size(0, 40);
+            this.lbl_Address.TabIndex = 2;
+            // 
+            // lbl_City
+            // 
+            this.lbl_City.AutoSize = true;
+            this.lbl_City.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lbl_City.Location = new System.Drawing.Point(671, 547);
+            this.lbl_City.Name = "lbl_City";
+            this.lbl_City.Size = new System.Drawing.Size(0, 40);
+            this.lbl_City.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1455, 755);
+            this.Controls.Add(this.lbl_City);
+            this.Controls.Add(this.lbl_Address);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Address;
+        private System.Windows.Forms.Label lbl_City;
+    }
+}
+
---
+++ A02_01/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A02_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lbl_Name.Text = "Abe Ableman";
+            lbl_Address.Text = "1234 Gone Away Dr.";
+            lbl_City.Text = "Cincinnati, Ohio, 98765";
+        }
+    }
+}
